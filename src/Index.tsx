7d30d62b8f2e23e7abd8cb14244b6521779dbab0
matthewlong <mattbasslong@gmail.com>
2026-05-04 16:04:32 -0600

    initial commit

---
+++ src/Index.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom/client'
+import {StrictMode} from "react"
+import {App} from './App'
+import './index.css'
+import React from "react"
+
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(
+        <StrictMode>
+            <App/>
+        </StrictMode>,
+    )
+} else {
+    console.error("Root element not found.")
+}
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+// vite.config.js
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    base: "/web/",
+    resolve: {
+        alias: {
+            '@': '/src' // Adjust the path if needed
+        }
+    },
+    esbuild: {
+        jsxFactory: 'React.createElement',
+        jsxInject: `import React from 'react'`,
+        // Specify JSX as the loader for .js files
+        jsx: 'react',
+    },
+    server: {
+        host: true,
+        allowedHosts: true
+    }
+});
+
+
